0271c92e8d12424c784a73980232354acced52af
ForSureNotKobe <j.porebski0@gmail.com>
2020-06-16 14:43:34 +0200

    Basic User with login/out

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,15 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @base_title = "ProjectApp"
+  end
+
+  test "should get home" do
+    get root_path
+    assert_response :success
+    assert_select "title", "#{@base_title}"
+  end
+
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,8 +1,13 @@
 ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
+require "minitest/reporters"
+Minitest::Reporters.use!
 
 class ActiveSupport::TestCase
+
+  include ApplicationHelper
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
@@ -10,4 +15,8 @@ class ActiveSupport::TestCase
   fixtures :all
 
   # Add more helper methods to be used by all tests here...
+
+  def is_logged_in?
+    !session[:user_id].nil?
+  end
 end
